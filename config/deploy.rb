0609391474c4a916760a05128762df109fa1f6a9
Ali Hassan Mirza <ali.hassan.mirzaa+100@gmail.com>
2020-11-26 05:23:11 -0500

    Changed remote URL

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :repo_url,        'git@github.com:ali-hassan/gullah.git'
+set :repo_url,        'git@github.com:Gullah-Marketplace/Marketplace.git'
 set :application,     'gullah'
 set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
